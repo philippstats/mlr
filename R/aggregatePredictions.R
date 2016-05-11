3425cfc3de024ab596fa5b6defa3f3a05ea2a08d
Philipp J Roesch <philipp.stats@gmail.com>
2016-05-11 09:41:02 +0200

    update and test aggregatePreditction (classif bpt resp spt prob is working)

--- R/aggregatePredictions.R
+++ R/aggregatePredictions.R
@@ -1,56 +1,3 @@
-'library(parallelMap)
-parallelStartMulticore(2)    # start in socket mode and create 2 processes on localhost
-f = function(i) i + 5     # define our job
-y = parallelMap(f, 1:2)   # like Rs Map but in parallel
-parallelStop() 
-
-for (i in seq_along(bls)) {
-    bl = bls[[i]]
-    model = train(bl, task)
-    base.models[[i]] = model
-    #
-    pred = predict(model, task = task)
-    probs[[i]] = getResponse(pred, full.matrix = TRUE)
-}
-
-library(mlr)
-tsk = subsetTask(iris.task, subset = c(46:50, 70:100, 130:150))
-tsk$env$data[1,1] = NA
-
-
-data = getTaskData(pid.task)
-data$CONT = 1
-tsk = makeClassifTask(data = data, target = "diabetes"); rm(data)
-tsk = subsetTask(tsk, 1:50)
-
-tsk = bh.task
-
-pt = "response"
-lrn0 = makeLearner("classif.rpart", predict.type = pt)
-lrn1 = makeLearner("classif.kknn", predict.type = pt)
-lrn2 = makeLearner("classif.svm", predict.type = pt)
-
-lrn0 = makeLearner("regr.rpart", predict.type = pt)
-lrn1 = makeLearner("regr.kknn", predict.type = pt)
-lrn2 = makeLearner("regr.svm", predict.type = pt)
-
-rm(tr0); rm(tr1)
-tr0 = train(lrn0, tsk)
-tr1 = train(lrn1, tsk)
-tr2 = train(lrn2, tsk)
-
-rm(pr0); rm(pr1)
-pr0 = predict(tr0, tsk)
-pr1 = predict(tr1, tsk)
-pr2 = predict(tr2, tsk)
-pr0
-pr1
-pr2
-
-rm(pred.list)
-pred.list = list(pr0, pr1, pr2)
-pred.list'
-
 #' Aggregate Predictions
 #' 
 #' Aggregate predicitons results by averaging (for \code{regr}, and  \code{classif} with prob) or mode ( \code{classif} with response). 
@@ -59,8 +6,7 @@ pred.list'
 #' @param pred.list [list of \code{Predictions}]\cr
 #' @export
 
-
-aggregatePredictions = function(pred.list) {
+aggregatePredictions = function(pred.list, spt = NULL) {
   # Check if "equal"
   x = lapply(pred.list, function(x) getTaskDescription(x))
   task.unequal = unlist(lapply(2:length(x), function(i) !all.equal(x[[1]], x[[i]])))
@@ -82,22 +28,41 @@ aggregatePredictions = function(pred.list) {
   id = pred1$data$id
   tr = pred1$data$truth
   pt = pred1$predict.type
+  if (is.null(spt)) spt = pt
+  
+  assertChoice(spt, choices = c("prob", "response"))
   ti = NA
-  pred.length = length(pred.list) # FIXME if any failures
+  pred.length = length(pred.list) 
   
   # Reduce results
+  # type = "classif"
   if (type == "classif") {
+    # pt = "prob"
     if (pt == "prob") {
+      # same method for spt response and prob
       preds = lapply(pred.list, getPredictionProbabilities, cl = td$class.levels)
-      y = Reduce("+", preds)/pred.length
+      y = Reduce("+", preds) / pred.length
+      if (spt == "response") {
+        y = factor(max.col(y), labels = td$class.levels)
+      }
+    # pt = "response"
     } else {
-      preds = as.data.frame(lapply(pred.list, getPredictionResponse))
-      y = factor(apply(preds, 1L, computeMode), td$class.levels)
+      if (spt == "response") {
+        preds = as.data.frame(lapply(pred.list, getPredictionResponse))
+        y = factor(apply(preds, 1L, computeMode), td$class.levels)
+      } else {
+        # rowiseRatio copied from Tong He (he said it's not the best solution). 
+        # This method should be rarely used, because pt = "response", 
+        # spt = "prob" should perfrom worse than setting pt = "prob" (due to 
+        # information loss when convertring probs to factors)
+        preds = as.data.frame(lapply(pred.list, function(x) x$data$response))
+        y = rowiseRatio(preds, td$class.levels, model.weight = NULL)
+      }
     }
-    return(mlr:::makePrediction(task.desc = td, rn, id = id, truth = tr, predict.type = pt, predict.threshold = NULL, y, time = ti))
-  } else { # type = "regr"
+  # type = "regr"
+  } else {
     preds = lapply(pred.list, getPredictionResponse)
     y = Reduce("+", preds)/pred.length
-    return(mlr:::makePrediction(task.desc = td, rn, id = id, truth = tr, predict.type = pt, predict.threshold = NULL, y, time = ti))
   }
+  return(makePrediction(task.desc = td, rn, id = id, truth = tr, predict.type = spt, predict.threshold = NULL, y, time = ti))
 }
--- tests/testthat/test_aggregatePredictions.R
+++ tests/testthat/test_aggregatePredictions.R
@@ -1,4 +1,11 @@
 context("aggregatePredictions")
+'
+source("~/Documents/Rdevelop/mlr/tests/testthat/helper_objects.R")
+keepPatternEnv("task")
+setwd("~/Documents/Rdevelop")
+library(devtools)
+load_all("mlr")
+'
 
 # classif
 pts = c("prob", "response")
@@ -6,21 +13,25 @@ tasks = list(binaryclass.task, multiclass.task)
 
 for (tsk in tasks) {
   for (pt in pts) {
-    lrn0 = makeLearner("classif.rpart", predict.type = pt)
-    lrn1 = makeLearner("classif.kknn", predict.type = pt)
-    lrn2 = makeLearner("classif.svm", predict.type = pt)
-
-    tr0 = train(lrn0, tsk)
-    tr1 = train(lrn1, tsk)
-    tr2 = train(lrn2, tsk)
-    pr0 = predict(tr0, tsk)
-    pr1 = predict(tr1, tsk)
-    pr2 = predict(tr2, tsk)
-    
-    pred.list = list(pr0, pr1, pr2)
-    p = aggregatePredictions(pred.list)
-    perf = performance(p) #%>% print
-    expect_that(perf < 1/3, is_true())
+    for (spt in pts) {
+      #messagef("This is %s, %s, %s \n", tsk$task.desc$id, pt, spt)
+      lrn0 = makeLearner("classif.rpart", predict.type = pt)
+      lrn1 = makeLearner("classif.kknn", predict.type = pt)
+      lrn2 = makeLearner("classif.kknn", k = 10, predict.type = pt)
+  
+      tr0 = train(lrn0, tsk)
+      tr1 = train(lrn1, tsk)
+      tr2 = train(lrn2, tsk)
+      pr0 = predict(tr0, tsk)
+      pr1 = predict(tr1, tsk)
+      pr2 = predict(tr2, tsk)
+      
+      pred.list = list(pr0, pr1, pr2)
+      p = aggregatePredictions(pred.list, spt = spt)
+      p #%>% print
+      perf = performance(p) #%>% print
+      expect_that(perf < 1/2, is_true())
+    }
   }
 }
 
@@ -28,9 +39,10 @@ for (tsk in tasks) {
 tasks = list(regr.task, regr.small.task, regr.num.task)
 lrn0 = makeLearner("regr.rpart")
 lrn1 = makeLearner("regr.kknn")
-lrn2 = makeLearner("regr.svm")
+lrn2 = makeLearner("regr.kknn", k = 10, predict.type = pt)
 
 for (tsk in tasks) {
+  #messagef("This is %s\n", tsk$task.desc$id)
   tr0 = train(lrn0, tsk)
   tr1 = train(lrn1, tsk)
   tr2 = train(lrn2, tsk)
@@ -40,6 +52,7 @@ for (tsk in tasks) {
   
   pred.list = list(pr0, pr1, pr2)
   p = aggregatePredictions(pred.list)
+  p #%>% print
   perf = performance(p) #%>% print
   expect_that(perf > 0.1, is_true())
 }
