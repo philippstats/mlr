4d66217362b9216022379b394190a4d78fd8327b
Philipp J Roesch <philipp.stats@gmail.com>
2016-05-13 11:25:54 +0200

    clean up

--- R/StackedLearner_predict.R
+++ R/StackedLearner_predict.R
@@ -1,19 +1,14 @@
 #' @export
-predictLearner.StackedLearner = function(.learner, .model, .newdata, ...) {
-  # FIXME actually only .learner$method is needed
+predictLearner.StackedLearner = function(.learner, .model, .newdata, ...) { # FIXME actually only .learner$method is needed
+  # prep
   use.feat = .model$learner$use.feat
-  # get predict.type from learner and super model (if available)
   sm.pt = .model$learner$predict.type
-  # get of predict type base learner
   bms.pt = unique(extractSubList(.model$learner$base.learners, "predict.type"))
-  # get task information (classif)
   td = .model$task.desc
   type = ifelse(td$type == "regr", "regr",
     ifelse(length(td$class.levels) == 2L, "classif", "multiclassif"))
   method = .learner$method
-  #
   # average/hill.climb
-  #
   if (method %in% c("average", "hill.climb2")) {
     pred.list = getStackedBaseLearnerPredictions(model = .model, newdata = .newdata, type = "pred")
     if (method == "average") {
@@ -23,35 +18,24 @@ predictLearner.StackedLearner = function(.learner, .model, .newdata, ...) {
       pred.list1 = expandPredList(pred.list, freq = freq)
       final.pred = aggregatePredictions(pred.list1, spt = sm.pt)
     }
-    if (sm.pt == "prob") {
-      return(as.matrix(getPredictionProbabilities(final.pred, cl = td$class.levels)))
-    } else {
-      return(final.pred$data$response) #getPredictionResponse?
-    }
-  #
   # stack.nocv stack.cv
-  #
   } else { 
     pred.data = getStackedBaseLearnerPredictions(model = .model, newdata = .newdata, type = "pred.data")
-    #so?:
     pred.data = as.data.frame(pred.data)
-
     if (use.feat) {
-      # feed pred.data into super model and we are done
-      feat = .newdata[, colnames(.newdata) %nin% td$target, drop = FALSE]
-      #TODO pred.data naming
-      predData = cbind(pred.data, feat)
-    } else {
-      predData = pred.data
-    }
+      #feat = .newdata[, colnames(.newdata) %nin% td$target, drop = FALSE] # TODO: there shouldn't be a target present in any case: feat = .newdata
+      #pred.data = cbind(pred.data, feat)
+      pred.data = cbind(pred.data, .newdata)
+    } 
     sm = .model$learner.model$super.model
-    messagef("Super model '%s' will use %s features and %s observations for prediction", sm$id, NCOL(predData), NROW(predData))
-    messagef("There are %s NA in 'predData'", sum(is.na(predData)))
-    pred = predict(sm, newdata = predData)
-    if (sm.pt == "prob") {
-      return(as.matrix(getPredictionProbabilities(pred, cl = td$class.levels)))
-    } else {
-      return(pred$data$response)
-    }
+    messagef("Super model '%s' will use %s features and %s observations for prediction", sm$id, NCOL(pred.data), NROW(pred.data))
+    messagef("There are %s NA in 'pred.data'", sum(is.na(pred.data)))
+    final.pred = predict(sm, newdata = pred.data)
+  }
+  # return 
+  if (sm.pt == "prob") {
+    return(as.matrix(getPredictionProbabilities(final.pred, cl = td$class.levels)))
+  } else {
+    return(final.pred$data$response) #getPredictionResponse?
   }
 }
