ee31aabdb392442653f24ecf329bab9034cba8a2
philippstats <philipp.stats@gmail.com>
2016-06-24 08:42:37 +0200

    naming things

--- R/StackedLearner_average.R
+++ R/StackedLearner_average.R
@@ -1,10 +1,12 @@
 # super simple averaging of base-learner predictions without weights. we should beat this
 averageBaseLearners = function(learner, task) {
-  bls = learner$base.learners
   id = learner$id
   save.on.disc = learner$save.on.disc
   save.preds = learner$save.preds 
-  # parallelMap
+  bls = learner$base.learners
+  bls.names = names(bls)
+
+  # parallelMap: train, predict
   parallelLibrary("mlr", master = FALSE, level = "mlr.stacking", show.info = FALSE)
   exportMlrOptions(level = "mlr.stacking")
   show.info = getMlrOption("show.info")
@@ -13,27 +15,25 @@ averageBaseLearners = function(learner, task) {
   #base.models = lapply(results, function(x) x[["base.models"]])
   base.models = lapply(results, function(x) x[["base.models"]])
   pred.data = lapply(results, function(x) try(getResponse(x[["pred"]], full.matrix = TRUE), silent = TRUE)) #QUEST: Return Predictions instead of data.frame!?
+  names(base.models) = bls.names
+  names(pred.data) = bls.names
+
   
-  #names(base.models) = names(bls)
-  names(base.models) = names(bls)
-  names(pred.data) = names(bls)
-  
-  #FIXME: I don't know if it is the nicest way to remove bls 
+  #FIXME: Ok way to remove bls1?
   #broke.idx.bm = which(unlist(lapply(base.models, function(x) any(class(x) == "FailureModel"))))
   broke.idx.pd1 = which(unlist(lapply(pred.data, function(x) anyNA(x)))) # if models is FailesModels and NAs are returend in a Prediction
   broke.idx.pd2 = which(unlist(lapply(pred.data, function(x) class(x) %nin% c("numeric", "factor", "data.frame")))) # if model fails and error message is returned it is not class numeric (regr, binary classif) nor data.frame (multiclassif)
-  #broke.idx = unique(broke.idx.bm, broke.idx.pd)
   broke.idx = unique(c(broke.idx.pd1, broke.idx.pd2))
   
   if (length(broke.idx) > 0) {
-    messagef("Base Learner %s is broken and will be removed\n", names(bls)[broke.idx])
+    messagef("Base Learner %s is broken and will be removed\n", bls.names[broke.idx])
     base.models = base.models[-broke.idx]
     pred.data = pred.data[-broke.idx]
   }
   
   if (save.preds == FALSE) pred.data = NULL
   
-  #list(method = "average", base.models = base.models, super.model = NULL,
+  # return
   list(method = "average", base.models = base.models, super.model = NULL,
        pred.train = pred.data)
 }
