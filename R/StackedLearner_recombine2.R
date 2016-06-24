d6e4ccc5e0df877ea57d2548742d593c851782de
philippstats <philipp.stats@gmail.com>
2016-06-24 08:43:53 +0200

    shift fcts

--- R/StackedLearner_recombine2.R
+++ R/StackedLearner_recombine2.R
@@ -1,3 +1,92 @@
+
+#' Retrain super learner on new test task
+#' 
+#' @param obj [\code{ResampleResult}]\cr Object using \code{StackedLearner} as Learner.
+#' @param super.learner [\code{Learner}]\cr New \code{super.learner} to apply.
+
+#'
+#retrainSuperLearner = function(models, super.learner, type, target, pred.train.is.pred = is.pred) {
+#  assertClass(models, "list")
+#  assertClass(super.learner, "Learner")
+#  # train new super.learner (we need level1 data and then train the super.learner model)
+#  #train.level1 = extractLevel1Task(obj)
+#  train.level1 = extractLevel1Task(models, type, target, pred.train.is.pred, as.task = TRUE)
+#  train.superlearner = vector("list", length = length(train.level1))
+#  for (i in seq_along(train.level1)) {
+#    train.superlearner[[i]] = train(super.learner, train.level1[[i]])
+#  }
+#  train.superlearner
+#}
+#'
+
+#' Extract level 1 data from stacking resample and create a task
+#' 
+#' @param obj [\code{ResampleResult}]\cr Object using \code{StackedLearner} as Learner.
+#' @param as.task [\code{logical(1)}]\cr Specify if task or data.frame should be returned. 
+
+
+#extractLevel1Task = function(models, type, target, pred.train.is.pred, as.task = TRUE) {
+#  #assertClass(obj, "ResampleResult")
+#  assertLogical(as.task)
+#  #type = obj$models[[1]]$task.desc$type
+#  #target = obj$models[[1]]$task.desc$target
+#
+#  #if (is.null(obj$models)) 
+#  #  stopf("Resample needs models. Use argument model = TRUE in your resample function.")
+#  datas = lapply(models, function(x) x$learner.model$pred.train)
+#  if (pred.train.is.pred) {
+#    for (f in folds)
+#    datas = lapply(seq_len(length(datas)), function(x) getResponse(datas[[x]], full.matrix = TRUE))
+#    if (type == "prob") { #remove first column for predict.type="prob"
+#      datas = lapply(datas, function(x) x[, -1])
+#    }
+#  }
+#  tasks = lapply(datas, function(d) createTask(type, d, target))
+#  if (as.task) {
+#    return(tasks)
+#  } else {
+#    return(datas)
+#  }
+#}
+
+#' Create a Classif or Regr Task
+#' 
+#' @param type [\code{character(1)}]\cr Use "classif" for Classification and "regr" for regression. 
+#' @param data [\code{data.frame}]\cr  Data to use.
+#' @param target [\code{character(1)}]\cr Target to use.
+ 
+createTask = function(type, data, target) {
+  if (type == "classif") {
+    task = makeClassifTask(data = data, target = target)
+  } else {
+    task = makeRegrTask(data = data, target = target)
+  }
+  task
+}
+
+getPreciseTaskType = function(task) {
+  type = getTaskType(task)
+  if (type == "classif" & length(task$task.desc$class.levels) > 2)
+    type = "multiclassif"
+  type
+}
+
+
+#' @export
+print.RecombinedResampleResult = function(x, ...) {
+  cat("Recombined Resample Result\n")
+  catf("Task: %s", x$task.id)
+  catf("Learner: %s", x$learner.id)
+  m = x$measure.test[, -1L, drop = FALSE]
+  Map(function(name, x, aggr) {
+    catf("%s.aggr: %.2f", name, aggr)
+    catf("%s.mean: %.2f", name, mean(x, na.rm = TRUE))
+    catf("%s.sd: %.2f", name, sd(x, na.rm = TRUE))
+  }, name = colnames(m), x = m, aggr = x$aggr)
+  catf("Runtime: %g", x$runtime)
+  invisible(NULL)
+}
+
 #' Create Predictions for Testing Set
 #' 
 #' @param i number of fold
